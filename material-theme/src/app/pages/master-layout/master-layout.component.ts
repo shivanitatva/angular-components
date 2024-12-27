8d9954969d0be601cd1d336cebde77cf7ee8bf1e
shivanitatva <shivani.ponda@tatvasoft.com>
2024-12-27 15:59:49 +0530

    Basic setup, added angular material and basic theme customization

---
+++ material-theme/src/app/pages/master-layout/master-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-master-layout',
+  imports: [],
+  templateUrl: './master-layout.component.html',
+  styleUrl: './master-layout.component.scss'
+})
+export class MasterLayoutComponent {
+
+}
